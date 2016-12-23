9ad5a84371b57a3e42dc7f411eac34b444f14237
J.T. Conklin <J.T.Conklin@Dell.Com>
2016-12-23 14:07:10 -0800

    Initial commit Copyright 2016, Dell Inc.
    From: e2ee819054b9f576b5ba4cab92406070e50d61dd

---
+++ inc/opx/sdi_led_internal.h
@@ -0,0 +1,71 @@
+/*
+ * Copyright (c) 2016 Dell Inc.
+ *
+ * Licensed under the Apache License, Version 2.0 (the "License"); you may
+ * not use this file except in compliance with the License. You may obtain
+ * a copy of the License at http://www.apache.org/licenses/LICENSE-2.0
+ *
+ * THIS CODE IS PROVIDED ON AN  *AS IS* BASIS, WITHOUT WARRANTIES OR
+ * CONDITIONS OF ANY KIND, EITHER EXPRESS OR IMPLIED, INCLUDING WITHOUT
+ *  LIMITATION ANY IMPLIED WARRANTIES OR CONDITIONS OF TITLE, FITNESS
+ * FOR A PARTICULAR PURPOSE, MERCHANTABLITY OR NON-INFRINGEMENT.
+ *
+ * See the Apache Version 2.0 License for specific language governing
+ * permissions and limitations under the License.
+ */
+
+/*
+ * filename: sdi_led_internal.h
+ */
+
+
+
+/**
+ * @file sdi_led_internal.h
+ * @brief Internal LED resource related declarations.
+ *
+ */
+
+#ifndef __SDI_LED_INTERNAL_H_
+#define __SDI_LED_INTERNAL_H_
+
+#include "std_error_codes.h"
+#include "std_type_defs.h"
+#include "sdi_entity.h"
+#include "sdi_led.h"
+
+/**
+ * @struct sdi_led_sensor_t
+ * @brief SDI LED Sensor Object for Callback Functions
+ * Each LED resource provides the following callbacks
+ * - led_on - callback function for turning on led
+ * - led_off - callback function for turning off led
+ */
+typedef struct {
+    /* LED Resource specific extensions start from here. */
+    t_std_error (*led_on)(void *resource_hdl); /**< callback function for turning
+    on led */
+    t_std_error (*led_off)(void *resource_hdl); /**< callback function for
+    turning off led */
+} sdi_led_sensor_t;
+
+/**
+ * @struct sdi_digital_display_led_t
+ * @brief SDI seven segment LED object for callback functions
+ * Each LED resource provides the following callbacks
+ *  - led_on - callback function for turning on digit led
+ *  - led_off - callback function for turning off digit led
+ *  - digit_led_set - callback function to set specified value in digit led
+ */
+typedef struct {
+    /** digital_display_led_on : callback function to turn-on digital LED */
+    t_std_error (*digital_display_led_on)(void *resource_hdl);
+
+    /** digital_display_led_off : callback function to turn-off digital LED */
+    t_std_error (*digital_display_led_off)(void *resource_hdl);
+
+    /** digital_display_led_set : callback function to set specified value on LED */
+    t_std_error (*digital_display_led_set)(void *resource_hdl, const char *display_string);
+} sdi_digital_display_led_t;
+
+#endif /* __SDI_LED_INTERNAL_H_ */
